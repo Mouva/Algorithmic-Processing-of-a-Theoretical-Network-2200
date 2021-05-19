b4acd5796c7952c2cf34d038859c137f41d4c889
mouva <38928342+Mouva@users.noreply.github.com>
2021-05-19 21:36:43 +0800

    numPaths base code

--- main/src/MyProject.java
+++ main/src/MyProject.java
@@ -1,5 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.*;
+import java.io.*;
 
 /**
  * @author Adrian Bedford 22973676
@@ -7,6 +9,10 @@ import java.util.Queue;
  */
 
 public class MyProject implements Project {
+    /**
+     * Breadth-first search
+     * Change for loops to iterate over elements, maybe improves quality
+     */
     public boolean allDevicesConnected(int[][] adjlist) {
         int length = adjlist.length;
         Queue<Integer> q = new ArrayDeque<>();
@@ -38,8 +44,43 @@ public class MyProject implements Project {
         return connected;
     }
 
+    /**
+     * Algorithm used?
+     */
     public int numPaths(int[][] adjlist, int src, int dst) {
-        // TODO
+        int length = adjlist.length;
+        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] results = new boolean[length];
+        int[] dist = new int[length];
+        int[] path = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            results[i] = false;
+        }
+        dist[src] = 0;
+        path[src] = 1;
+
+        q.add(src);
+        results[src] = true;
+        while (!q.isEmpty()) {
+            int current = q.remove();
+            results[current] = true;
+
+            for (int x : adjlist[current]) {
+                if (!results[x]) {
+                    q.add(x);
+                    results[x] = true;
+                }
+                if (dist[x] > dist[current] + 1) {
+                    dist[x] = dist[current] + 1;
+                    path[x] = path[current];
+                }
+                else if (dist[x] == dist[current]) {
+                    path[x] += path[current];
+                }
+            }   
+        }
+
         return 0;
     }
 
