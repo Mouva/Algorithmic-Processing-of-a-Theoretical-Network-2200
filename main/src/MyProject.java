626c3dd1d6a12f7c1f6cc47b28955879d8fc989d
mouva <38928342+Mouva@users.noreply.github.com>
2021-05-19 18:10:08 +0800

    allDevicesConnected body

--- main/src/MyProject.java
+++ main/src/MyProject.java
@@ -8,12 +8,26 @@ import java.util.Queue;
 
 public class MyProject implements Project {
     public boolean allDevicesConnected(int[][] adjlist) {
-        Queue<Integer> q = new ArrayDeque<>();
+        Queue<Integer> q = new ArrayDeque<>(0);
         int length = adjlist.length;
         boolean[] results = new boolean[length];
-        
-        return false;
+        boolean connected = false;
+
+        for (int i = 0; i < length; i++) {
+            results[i] = false;
+        }
+
+        while (!q.isEmpty()) {
+            int current = q.remove();
+            results[current] = true;
+            for (int i = 0; i < length; i++) {
+                if (!results[adjlist[current][i]]) {
+                    q.add(adjlist[current][i]);
+            }
+        }
+        return connected;
     }
+}
 
     public int numPaths(int[][] adjlist, int src, int dst) {
         // TODO
